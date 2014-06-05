fe87d07deca3b1db734f3ca5967196a0188400c0
wshaolin <wshaolin88@126.com>
2014-06-05 16:46:09 +0800

    Initial Commit

---
+++ SLFMDBUtil/SLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SLAppDelegate.h
+//  SLFMDBUtil
+//
+//  Created by wshaolin on 14-6-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
